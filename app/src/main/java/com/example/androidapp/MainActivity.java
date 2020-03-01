af9d42cbb5319be36c98e53c669831823eebea91
Jamie <robb79jamie@outlook.com>
2020-03-01 22:26:07 +0000

    Initial commit

---
+++ app/src/main/java/com/example/androidapp/MainActivity.java
@@ -0,0 +1,85 @@
+package com.example.androidapp;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        Button weatherNav = findViewById(R.id.weatherButton);
+        weatherNav.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent weatherActivity = new Intent(MainActivity.this, WeatherActivity.class);
+                startActivity(weatherActivity);
+                Log.i("Switch Activity", "Opened Weather Activity");
+            }
+        });
+
+        final Spinner locationSpinner = findViewById(R.id.locationSpinnerID);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.mainLocations, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        locationSpinner.setAdapter(adapter);
+
+        //Listener which changes the info text when a new location is selected
+        locationSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            TextView locationInfo = findViewById(R.id.locationInfoText);
+            ImageView mullImage = findViewById(R.id.mullPic);
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String locationChooser = locationSpinner.getSelectedItem().toString();
+                if(locationChooser.equals("Tobermory")){
+                    locationInfo.setText(R.string.tobermory_info);
+                    mullImage.setImageResource(R.drawable.mulltob);
+                    Log.i("Change location", locationChooser + " selected");
+                } else if(locationChooser.equals("Salen")){
+                    locationInfo.setText(R.string.salen_info);
+                    mullImage.setImageResource(R.drawable.mullsalen);
+                    Log.i("Change location", locationChooser + " selected");
+                }  else if(locationChooser.equals("Craignure")){
+                    locationInfo.setText(R.string.craignure_info);
+                    mullImage.setImageResource(R.drawable.mullcraignure);
+                    Log.i("Change location", locationChooser + " selected");
+                }  else if(locationChooser.equals("Iona")){
+                    locationInfo.setText(R.string.iona_info);
+                    mullImage.setImageResource(R.drawable.mulliona);
+                    Log.i("Change location", locationChooser + " selected");
+                }  else if(locationChooser.equals("Staffa")){
+                    locationInfo.setText(R.string.staffa_info);
+                    mullImage.setImageResource(R.drawable.mullstaffa);
+                    Log.i("Change location", locationChooser + " selected");
+                }  else if(locationChooser.equals("Calgary Bay")){
+                    locationInfo.setText(R.string.calgary_info);
+                    mullImage.setImageResource(R.drawable.mullcalgary);
+                    Log.i("Change location", locationChooser + " selected");
+                }
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                locationInfo.setText("Select a location above to learn more...");
+                mullImage.setImageResource(R.drawable.mull);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/androidapp/WeatherActivity.java
@@ -0,0 +1,130 @@
+package com.example.androidapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.text.DecimalFormat;
+
+public class WeatherActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_activity);
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+
+        StrictMode.setThreadPolicy(policy);
+
+        try {
+            JSONObject weatherjson = readJsonFromUrl("https://api.openweathermap.org/data/2.5/forecast?id=2641108&appid=85ce4dba63ea014d404782678b49dc33");
+            if(weatherjson.getInt("cod") == 200) {
+                try {
+                    DecimalFormat df = new DecimalFormat("0.00");
+
+                    TableLayout weatherTable = findViewById(R.id.weatherTable);
+
+                    String jsonString = weatherjson.getString("list");
+                    JSONArray weatherData = new JSONArray(jsonString);
+                    for(int i=0; i<30; i++) {
+                        JSONObject weatherPrimary = weatherData.getJSONObject(i);
+                        JSONObject weatherMain = weatherPrimary.getJSONObject("main");
+                        JSONArray weatherSecondaryTemporary = weatherPrimary.getJSONArray("weather");
+                        JSONObject weatherSecondary = weatherSecondaryTemporary.getJSONObject(0);
+                        JSONObject weatherWind = weatherPrimary.getJSONObject("wind");
+                        String weatherTime = weatherPrimary.getString("dt_txt");
+                        JSONObject weatherRain = new JSONObject();
+                        if(weatherSecondary.getString("main").equals("Rain"))
+                        {
+                            weatherRain = weatherPrimary.getJSONObject("rain");
+                        } else if(weatherSecondary.getString("main").equals("Snow"))
+                        {
+                            weatherRain = weatherPrimary.getJSONObject("snow");
+                        } else
+                        {
+                            weatherRain = null;
+                        }
+
+                        TableRow weatherRow = new TableRow(getApplicationContext());
+
+                        TextView dateText = new TextView(getApplicationContext());
+                        TextView windSpeedText = new TextView(getApplicationContext());
+                        TextView windDirText = new TextView(getApplicationContext());
+                        TextView precipitationText = new TextView(getApplicationContext());
+                        TextView tempText = new TextView(getApplicationContext());
+
+                        dateText.setText(weatherTime);
+                        double windSpeed = Double.parseDouble(weatherWind.getString("speed"));
+                        windSpeedText.setText(df.format(windSpeed*1.944));
+                        windDirText.setText(weatherWind.getString("deg"));
+                        if(weatherRain != null) {
+                            precipitationText.setText(weatherSecondary.getString("description") + ", " + weatherRain.getString("3h") + "mm");
+                        } else {
+                            precipitationText.setText("None");
+                        }
+                        String tempKelvin = weatherMain.getString("temp").substring(0, 3);
+                        double tempTemp = Double.parseDouble(tempKelvin) - 273;
+                        tempText.setText(Long.toString(Math.round(tempTemp)));
+
+                        weatherRow.addView(dateText);
+                        weatherRow.addView(windSpeedText);
+                        weatherRow.addView(windDirText);
+                        weatherRow.addView(precipitationText);
+                        weatherRow.addView(tempText);
+
+                        weatherTable.addView(weatherRow);
+                    }
+                } catch(JSONException e) {
+                    Log.e("Error converting data", e.toString());
+                }
+            } else {
+                Log.e("Error connecting to API", weatherjson.getString("cod"));
+            }
+        } catch(IOException | JSONException e) {
+            Log.e("Error getting weather", e.toString());
+        }
+    }
+
+    private static String readAll(Reader rd) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        int cp;
+        while ((cp = rd.read()) != -1) {
+            sb.append((char) cp);
+        }
+        return sb.toString();
+    }
+
+    public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
+        InputStream is = new URL(url).openStream();
+        try {
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+            String jsonText = readAll(rd);
+            JSONObject json = new JSONObject(jsonText);
+            return json;
+        } finally {
+            is.close();
+        }
+    }
+}
+
+
